57221decb3ad2d650da03185aa3482a077881009
SahanaH84 <112095932+SahanaH84@users.noreply.github.com>
2022-10-26 21:32:55 +0530

    Add files via upload

---
+++ src/CBstream.java
@@ -0,0 +1,44 @@
+import java.io.*;
+public class CBstream {
+    public static void main(String[] args)throws IOException
+    {
+        FileReader sourceStream = null;
+        try {
+            sourceStream = new FileReader("C:\\Users\\pranav\\OneDrive\\Desktop\\IN.txt");
+            int temp;
+            while ((temp = sourceStream.read()) != -1)
+            {
+            System.out.println((char)temp);
+            Thread.sleep(100);
+            }
+            System.out.print("Program successfully executed");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally {
+            if (sourceStream != null)
+                sourceStream.close();
+            System.out.println("\nCharacter Stream Over\n===================================");
+        }
+        FileInputStream sourceStream1 = null;
+        FileOutputStream targetStream1 = null;
+        try {
+            sourceStream1 = new FileInputStream("C:\\Users\\pranav\\OneDrive\\Desktop\\OUT.txt");
+            targetStream1 = new FileOutputStream("C:\\Users\\pranav\\OneDrive\\Desktop\\target.txt");
+            int temp;
+            while ((temp = sourceStream1.read()) != -1)
+                targetStream1.write((byte)temp);
+          System.out.print("Program successfully executed");
+        }
+        finally {
+            if (sourceStream1 != null)
+                sourceStream1.close();
+ 
+            if (targetStream1 != null)
+                targetStream1.close();
+            System.out.println("\nByte Stream Over\n===================================");
+        }
+    } 
+}
